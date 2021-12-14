b7af0b14dec2b1c4e3952bf3eba96716592ab48e
Infi <wingit@geist.ga>
2021-12-14 21:17:11 +0100

    feat: Express base

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,27 @@
-console.log("Hello World")
+import express from "express"
+import { config as dotenvConfig } from "dotenv"
+import cors from "cors"
+import helmet from "helmet"
+
+dotenvConfig()
+
+const app = express()
+
+app.use(cors())
+app.use(helmet())
+
+app.get("/", (req, res) => {
+    res.json({
+        status: "ok",
+    })
+})
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: "Not Found",
+    })
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT}`)
+})
