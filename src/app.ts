a0d7408ec15d83e7aaeaa0c673eaaefea57329a2
Infi <wingit@geist.ga>
2022-05-31 23:25:36 +0200

    feat: error handling for invalid request bodies

--- src/app.ts
+++ src/app.ts
@@ -19,11 +19,20 @@ export const db = await mongoose.connect(process.env.MONGO_URI, process.env.MONG
 } : {})
 export const app = express()
 
+
+app.use(express.json())
+app.use((err, req, res, next) => {
+    if (err) {
+        res.status(400).json({
+            error: err.message ?? "Incomprehensible request body",
+        })
+    }
+})
+
 app.use(cors())
 app.use(helmet({
     crossOriginResourcePolicy: false
 }))
-app.use(express.json())
 
 app.get("/", (req, res) => {
     res.json({
@@ -38,14 +47,14 @@ app.use("/file", FileRouter)
 
 app.use((req, res, next) => {
     res.status(404).json({
-        message: "Not Found",
+        error: "Not Found",
     })
 })
 
 app.use((err, req, res, next) => {
     console.error(err)
     res.status(500).json({
-        message: "Internal Server Error",
+        error: "Internal Server Error",
     })
 })
 
